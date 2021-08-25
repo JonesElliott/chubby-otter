cada97e1ba55c6ef348ef7903e369e670d746cca
Elliott Jones <jones.elliott@outlook.com>
2021-08-25 13:15:42 -0700

    Added dynamic slider labels to update on slider input

--- projects/hostile-design-concept/script/hostile.js
+++ projects/hostile-design-concept/script/hostile.js
@@ -27,8 +27,14 @@ function createFirstNameForm() {
 // Event listener for first name button click
 document.getElementById("fn-submit").addEventListener("click", function () {
   var inputElement = document.getElementById("fn-input");
+
+  // LOGGING TO REMVOE
   console.log(`You have ${inputElement.value} letters in your first name`);
-  generateSliders(inputElement.value);
+
+  // Check if value is a number before proceeding
+  if (!isNaN(inputElement.value)) {
+    generateSliders(inputElement.value);
+  }
 });
 
 function removeElementsByID(elementID) {
@@ -40,11 +46,10 @@ function removeElementsByID(elementID) {
 
 // Placeholder logic
 function generateSliders(input) {
+  // LOGGING TO REMVOE
   console.log(input);
-  if (Number.isNaN(input)) {
-    return;
-  }
 
+  // Clean up div that will be used to generate the sliders in
   removeElementsByID("hostile-form-div");
 
   // Set instructions
@@ -53,6 +58,7 @@ function generateSliders(input) {
   question.innerText = "Please enter your name below";
   container.prepend(question);
 
+  // Generate X number of slider & label elements
   for (let i = 0; i < input; i++) {
     var newSlider = document.createElement("input");
     newSlider.setAttribute("type", "range");
@@ -60,15 +66,28 @@ function generateSliders(input) {
     newSlider.setAttribute("max", "25");
     newSlider.setAttribute("value", "0");
     newSlider.setAttribute("class", "slider");
-    newSlider.setAttribute("id", `range${i}`);
+    newSlider.setAttribute("id", `range-${i}`);
 
     var newLabel = document.createElement("label");
     newLabel.setAttribute("class", "slider-label");
     newLabel.setAttribute("id", `slider-label-${i}`);
-    newLabel.innerText = newSlider.value;
 
     container.appendChild(newSlider);
     container.appendChild(newLabel);
-  }
 
+    // On slider input, update corresponding label
+    newSlider.oninput = function () {
+        updateSliderLabel(this)
+    };
+  }
+}
+
+// Update label dynamically to reflect slider value
+function updateSliderLabel(element) {
+    // Get slider element ID No.
+    var sliderIDNo = element.id.split('range-').pop();
+    // Get related label element based on slider ID No.
+    var label = document.getElementById(`slider-label-${sliderIDNo}`);
+    // Update label based on slider value
+    label.innerText = element.value;
 }
